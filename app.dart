052503fa856d437f681b26df53710a703c6d329b
Mr.Presh <preciousokeypraise@gmail.com>
2025-08-21 05:39:30 +0100

    Just adding guy

--- app.dart
+++ app.dart
@@ -7,25 +7,25 @@ class Num{
 void main(){
   var n;
   stdout.writeln("Hello there, what is your name?");
-  String? name = stdin.readLineSync();
-  print(name);
+  String? name1 = stdin.readLineSync();
+  print(name1);
   int number;
   number = n?.num ?? 0;
   print(number);
 
-  int? age;
-  print(age ??= 0);
+  int? age1;
+  print(age1 ??= 0);
 
   int x = 100;
   var result = x % 2 == 0 ? 'Even' : 'Odd';
   print(result);
 
   stdout.writeln("Hello there, what's your name?");
-  String? name = stdin.readLineSync();
-  stdout.writeln("Alright $name, how old are you?");
+  String? name2 = stdin.readLineSync();
+  stdout.writeln("Alright $name2, how old are you?");
   String? age_string = stdin.readLineSync();
   String age_str = age_string!;
-  int age = int.parse(age_str);
-  var check = 18 - age >= 1 ? 'Please, you\'re too young for this program...try again when you\'re 18 and above' : 'Oh ok, welcome aboard :)';
+  int age2 = int.parse(age_str);
+  var check = 18 - age2 >= 1 ? 'Please, you\'re too young for this program...try again when you\'re 18 and above' : 'Oh ok, welcome aboard :)';
   print(check);
 }
