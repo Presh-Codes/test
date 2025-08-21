82a611fd2558cccfb3ae05a856694e24d6fc7a90
Mr.Presh <preciousokeypraise@gmail.com>
2025-08-21 05:37:48 +0100

    op: just adding bruh

--- app.dart
+++ app.dart
@@ -15,4 +15,17 @@ void main(){
 
   int? age;
   print(age ??= 0);
+
+  int x = 100;
+  var result = x % 2 == 0 ? 'Even' : 'Odd';
+  print(result);
+
+  stdout.writeln("Hello there, what's your name?");
+  String? name = stdin.readLineSync();
+  stdout.writeln("Alright $name, how old are you?");
+  String? age_string = stdin.readLineSync();
+  String age_str = age_string!;
+  int age = int.parse(age_str);
+  var check = 18 - age >= 1 ? 'Please, you\'re too young for this program...try again when you\'re 18 and above' : 'Oh ok, welcome aboard :)';
+  print(check);
 }
