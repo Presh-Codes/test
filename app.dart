d585b9f21fd9e8a30a3eef2b532c253fccc33ae2
Mr.Presh <preciousokeypraise@gmail.com>
2025-08-21 03:29:45 +0100

    chore: testing out...again

--- app.dart
+++ app.dart
@@ -1,7 +1,18 @@
 import 'dart:io';
 
+class Num{
+  int num = 10;
+}
+
 void main(){
+  var n;
   stdout.writeln("Hello there, what is your name?");
   String? name = stdin.readLineSync();
   print(name);
+  int number;
+  number = n?.num ?? 0;
+  print(number);
+
+  int? age;
+  print(age ??= 0);
 }
