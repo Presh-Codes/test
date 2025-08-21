41896372afa5a893960b69b941ebfb3d4c48b89a
Mr.Presh <preciousokeypraise@gmail.com>
2025-08-21 23:23:46 +0100

    Just another push

--- app.dart
+++ app.dart
@@ -5,29 +5,29 @@ class Num{
 }
 
 void main(){
-  // var n;
-  // stdout.writeln("Hello there, what is your name?");
-  // String? name1 = stdin.readLineSync();
-  // print(name1);
-  // int number;
-  // number = n?.num ?? 0;
-  // print(number);
+  var n;
+  stdout.writeln("Hello there, what is your name?");
+  String? name1 = stdin.readLineSync();
+  print(name1);
+  int number;
+  number = n?.num ?? 0;
+  print(number);
 
-  // int? age1;
-  // print(age1 ??= 0);
+  int? age1;
+  print(age1 ??= 0);
 
-  // int x = 100;
-  // var result = x % 2 == 0 ? 'Even' : 'Odd';
-  // print(result);
+  int x = 100;
+  var result = x % 2 == 0 ? 'Even' : 'Odd';
+  print(result);
 
-  // stdout.writeln("Hello there, what's your name?");
-  // String? name2 = stdin.readLineSync();
-  // stdout.writeln("Alright $name2, how old are you?");
-  // String? age_string = stdin.readLineSync();
-  // String age_str = age_string!;
-  // int age2 = int.parse(age_str);
-  // var check = 18 - age2 >= 1 ? 'Please, you\'re too young for this program...try again when you\'re 18 and above' : 'Oh ok, welcome aboard :)';
-  // print(check);
+  stdout.writeln("Hello there, what's your name?");
+  String? name2 = stdin.readLineSync();
+  stdout.writeln("Alright $name2, how old are you?");
+  String? age_string = stdin.readLineSync();
+  String age_str = age_string!;
+  int age2 = int.parse(age_str);
+  var check = 18 - age2 >= 1 ? 'Please, you\'re too young for this program...try again when you\'re 18 and above' : 'Oh ok, welcome aboard :)';
+  print(check);
 
   int number2 = 101;
   if (number2 % 2 == 0){
@@ -50,4 +50,16 @@ void main(){
     default:
       print("Confused");
   }
+
+  for (var i = 1; i <= 10; ++i){
+    print(i);
+  }
+
+  var number3 = [11, 12, 13, 14, 15];
+  var number4 = [16, 17, 18, 19, 20];
+  for (var n in number3){
+    print(n);
+  }
+
+  number4.forEach( (n) => print(n) );
 }
